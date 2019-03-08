1dfb8ab473ff95d4f299df526664b18a9e5d1f49
sirleyalmeida <sirleyas90@gmail.com>
2019-03-07 23:49:08 -0300

    css com nav ajustada, container map adicionado ícones dos países

--- src/main.js
+++ src/main.js
@@ -1,81 +1,64 @@
+window.onload = function () {
+
+};
+
+let getDataPeru = () => {
+    return WORLDBANK.PER["indicators"];
+}
+
+getDataPeru();
+// console.log(getDataPeru());
+
+let getDataChile = () => {
+    return WORLDBANK.CHL["indicators"];
+}
+getDataChile()
+
+let getDataMexico = () => {
+    return WORLDBANK.MEX["indicators"];
+}
+getDataMexico()
+
+let getDataBrazil = () => {
+    return WORLDBANK.BRA["indicators"];
+};
+getDataBrazil()
+console.log(getDataBrazil());
+
+let getBR = WORLDBANK.BRA["indicators"];
+console.log(getBR)
+
+let mapDataBrazil = getBR.filter(({
+    countryCode, countryName, data, indicatorCode, indicatorName,
+}) => [`${countryName} ${data} ${indicatorName} `, countryCode - indicatorCode]);
+console.log(mapDataBrazil);
+
+
+
+let arrBr = mapDataBrazil.map(({ data, }) => [`${data}`]);
+console.log(arrBr)
+
 
 // let showElementsTab = (e) => {
 //     let tabSelect = e.target.dataset.tabenable;
 //     let jobBr = document.querySelector("#jobBr");
 //     let unemployedBr = document.querySelector("#unemployedBr");
 //     let educationBr = document.querySelector("#educationBr");
-//     let jobMex = document.querySelector("#jobMex");
-//     let unemployedMex = document.querySelector("#unemployedMex");
-//     let educationMex = document.querySelector("#educationMex");
-//     let jobCl = document.querySelector("#jobCl");
-//     let unemployedCl = document.querySelector("#unemployedCl");
-//     let educationCl = document.querySelector("#educationCl");
-//     let jobPe = document.querySelector("#jobPe");
-//     let unemployedPe = document.querySelector("#unemployedPe");
-//     let educationPe = document.querySelector("#educationPe");
 
 //     if (tabSelect === "tabJob") {
 //         jobBr.style.display = "block";
-//         jobMex.style.display = "block";
-//         jobCl.style.display = "block";
-//         jobPe.style.display = "block";
 //         unemployedBr.style.display = "none";
-//         unemployedMex.style.display = "none";
-//         unemployedCl.style.display = "none";
-//         unemployedPe.style.display = "none";
 //         educationBr.style.display = "none";
-//         educationMex.style.display = "none";
-//         educationCl.style.display = "none";
-//         educationPe.style.display = "none";
 //     } else if (tabSelect === "tabUnemployed") {
 //         jobBr.style.display = "none";
-//         jobMex.style.display = "none";
-//         jobCl.style.display = "none";
-//         jobPe.style.display = "none";
 //         unemployedBr.style.display = "block";
-//         unemployedMex.style.display = "block";
-//         unemployedCl.style.display = "block";
-//         unemployedPe.style.display = "block";
 //         educationBr.style.display = "none";
-//         educationMex.style.display = "none";
-//         educationCl.style.display = "none";
-//         educationPe.style.display = "none";
 //     } else if (tabSelect === "tabEducation") {
 //         jobBr.style.display = "none";
-//         jobMex.style.display = "none";
-//         jobCl.style.display = "none";
-//         jobPe.style.display = "none";
 //         unemployedBr.style.display = "none";
-//         unemployedMex.style.display = "none";
-//         unemployedCl.style.display = "none";
-//         unemployedPe.style.display = "none";
 //         educationBr.style.display = "block";
-//         educationMex.style.display = "block";
-//         educationCl.style.display = "block";
-//         educationPe.style.display = "block";
 //     }
 // }
-
-let showElementsTab = (e) => {
-    let tabSelect = e.target.dataset.tabenable;
-    let jobBr = document.querySelector("#jobBr");
-    let unemployedBr = document.querySelector("#unemployedBr");
-    let educationBr = document.querySelector("#educationBr");
-
-    if (tabSelect === "tabJob") {
-        jobBr.style.display = "block";
-        unemployedBr.style.display = "none";
-        educationBr.style.display = "none";
-    } else if (tabSelect === "tabUnemployed") {
-        jobBr.style.display = "none";
-        unemployedBr.style.display = "block";
-        educationBr.style.display = "none";
-    } else if (tabSelect === "tabEducation") {
-        jobBr.style.display = "none";
-        unemployedBr.style.display = "none";
-        educationBr.style.display = "block";
-    }
-}
 
 // let tagsBr = () => {
 //     let jobBr = document.querySelector("#jobBr");
