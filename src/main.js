347f393e5065beeac5cd74fae80b3cdf305bc7ad
sirleyalmeida <sirleyas90@gmail.com>
2019-03-09 22:30:28 -0300

    add section de pesquisar filtrando os indicadores, o ano e o país

--- src/main.js
+++ src/main.js
@@ -2,43 +2,61 @@ window.onload = function () {
 
 };
 
-// let getDataPeru = () => {
-//     return WORLDBANK.PER["indicators"];
+let getDataMexico = () => {
+    return WORLDBANK.MEX["indicators"];
+}
+getDataMexico()
+
+let getDataBrazil = () => {
+    return WORLDBANK.BRA["indicators"];
+};
+getDataBrazil()
+console.log("indicadores do Brasil", getDataBrazil());
+
+let getDataPeru = () => {
+    return WORLDBANK.PER["indicators"];
+}
+getDataPeru();
+// console.log(getDataPeru());
+
+let getDataChile = () => {
+    return WORLDBANK.CHL["indicators"];
+}
+getDataChile()
+
+
+// function showDataBrazil() {
+//     let indicatorsBrazil = document.getElementById("products-div");
+
+//     productDIv.innerHTML = `
+//     ${getProducts().map((produto) => `
+//      <div class="product-item">
+//       <img src="${produto["product"]["images"][0]}" class="product-img" />
+//       <div class="text-name">
+//        <h3 class="product-name">${produto["product"]["name"]}</h3>
+//       </div>
+//       <div class="text-price">
+//        <p class="product-price">${Number(produto["product"]["price"]["value"]).toLocaleString("pt-br", { minimumFractionDigits: 2, style: "currency", currency: "BRL" })}</p>
+//       </div>
+//      </div>
+//     `).join("")}
+//   `
 // }
 
-// getDataPeru();
-// // console.log(getDataPeru());
-
-// let getDataChile = () => {
-//     return WORLDBANK.CHL["indicators"];
-// }
-// getDataChile()
-
-// let getDataMexico = () => {
-//     return WORLDBANK.MEX["indicators"];
-// }
-// getDataMexico()
-
-// let getDataBrazil = () => {
-//     return WORLDBANK.BRA["indicators"];
-// };
-// getDataBrazil()
-// console.log(getDataBrazil());
-
-let getBR = WORLDBANK.BRA["indicators"];
-console.log(getBR)
+// let getBR = WORLDBANK.BRA["indicators"];
+// console.log(getBR)
 
 
-let mapDataBrazil = getBR.filter(({
-    countryCode, countryName, data, indicatorCode, indicatorName,
-}) => [`${countryName} ${data} ${indicatorName} `, countryCode - indicatorCode]);
-console.log(mapDataBrazil);
+// let mapDataBrazil = getBR.filter(({
+//     countryCode, countryName, data, indicatorCode, indicatorName,
+// }) => [`${countryName} ${data} ${indicatorName} `, countryCode - indicatorCode]);
+// console.log(mapDataBrazil);
 
-let objetoBrazil = JSON.stringify(mapDataBrazil);
-console.log(objetoBrazil)
+// let objetoBrazil = JSON.stringify(mapDataBrazil);
+// console.log(objetoBrazil)
 
-let teste = mapDataBrazil[109]["indicatorName"];
-console.log(teste)
+// let teste = mapDataBrazil[109]["indicatorName"];
+// console.log(teste)
 
 
 // let arrBr = mapDataBrazil.map(({ data, }) => [`${data}`]);
